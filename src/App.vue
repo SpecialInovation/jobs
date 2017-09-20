092cc711deb299044bccb580191d4d2cb65e603a
Gin <gin.lance.inside@hotmail.com>
2017-09-20 23:36:48 +0800

    upd: standard the font

--- src/App.vue
+++ src/App.vue
@@ -29,6 +29,7 @@ export default {
 
 <style>
 body {
+  font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
   margin: 0;
   min-width: 1200px;
 }
